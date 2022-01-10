cec3a36683ae1c5fe9c2e75bc68526c103238617
Rqwannn <mohwannn1@gmail.com>
2022-01-10 16:29:53 +0700

    add list article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
   
-  def detail
+  def show
     # set @ agar bisa di gunakan di erb file
     @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "/about", to: "pages#about"
-  resource :article, only: [:show]
-  get "/article/:id", to: "articles#detail", as: 'articleDetail'
+  resources :articles, only: [:show, :index]
+  # get "/article/:id", to: "articles#detail", as: 'detail_article'
 end
